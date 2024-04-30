ded9b5db300c2014482ef3c7ad6c76e7431d0ded
CaueFenn <165732840+CaueFenn@users.noreply.github.com>
2024-04-29 21:34:22 -0300

    Add files via upload

---
+++ principal/Principal.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package principal;
+
+
+import visao.FrmMenuPrincipal;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        FrmMenuPrincipal objetoTela = new FrmMenuPrincipal();
+        objetoTela.setVisible(true);
+        
+        
+        
+        
+    }
+}
